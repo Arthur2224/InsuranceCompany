d2207188a7be6c0d7093ca85ce9d9982388a9d00
Arthur224 <nervnijkrot@gmail.com>
2023-10-23 17:41:21 +0300

    added workable method to Clear Button...

--- src/main/java/com/example/insurance/HelloController.java
+++ src/main/java/com/example/insurance/HelloController.java
@@ -1,14 +1,34 @@
 package com.example.insurance;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 public class HelloController {
     @FXML
-    private TextField PhoneTextField;
+    public TextField PhoneTextField;
+    @FXML
     public VBox vboxWithBorder;
+    @FXML
+    public GridPane GridPanel;
+    @FXML
+    public DatePicker Calendar;
 
     // The method to handle the InputMethodTextChanged event
+    @FXML
+    protected void ClearTextFields() {
+        for (Node node : GridPanel.getChildren()) {
+            if (node instanceof TextInputControl || node instanceof DatePicker) {
+                if (node instanceof TextInputControl) {
+                    ((TextInputControl) node).clear();
+                } else if (node instanceof DatePicker) {
+                    ((DatePicker) node).setValue(null);
+                }
+            }
+        }
 
-}
+        ;
+
+    }}
