6a2799ef6d53952166db445a20a2ce472f6bf506
Arthur224Laptop <nervnijkrot@gmail.com>
2023-10-24 14:46:18 +0300

    Added SignUp form. added function(button-image) to switch to new form

--- src/main/java/com/example/insurance/HelloController.java
+++ src/main/java/com/example/insurance/HelloController.java
@@ -1,23 +1,38 @@
 package com.example.insurance;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
+    //Creating a graphic (image)
+
+
+
+
     @FXML
     public TextField PhoneTextField;
     @FXML
     Text coloredText = new Text();
     @FXML
-    public VBox vboxWithBorder;
+    public Button SignUp;
     @FXML
     public GridPane GridPanel;
     @FXML
     public DatePicker Calendar;
+
+
 
     // The method to handle the InputMethodTextChanged event
     @FXML
@@ -30,11 +45,29 @@ public class HelloController {
                     ((DatePicker) node).setValue(null);
                 }
             }
-        };
+        };}
+        @FXML
+        protected  void SwitchToSignUpScene() {
+            try {
+                // Load the new scene
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("SignUp.fxml"));
+                Parent root = loader.load();
+
+                // Create the stage and set the new scene
+                Stage stage = (Stage) SignUp.getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
 
 
 
+
+
+
+    ;
     }
-
-
-    ;}
