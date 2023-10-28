e92591f56b8e457d31aec0a7c80d3832360351ef
Arthur224Laptop <nervnijkrot@gmail.com>
2023-10-28 12:11:24 +0300

    added machine insurance tab

--- src/main/java/com/example/insurance/MainMenuController.java
+++ src/main/java/com/example/insurance/MainMenuController.java
@@ -1,4 +1,22 @@
 package com.example.insurance;
 
-public class MainMenuController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController implements Initializable {
+    @FXML
+    public ChoiceBox<String> typeOfInsurance;
+   @FXML
+    public ChoiceBox<String> termOfInsurance;
+    private String[] types={"casco","simple"};
+    @Override
+    public void initialize(URL arg0, ResourceBundle arf1){
+            typeOfInsurance.getItems().addAll(types);
+    }
+
 }
